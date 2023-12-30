3af374aebb35ff04c5b70dfa60c01abd98a65df1
mariaclarabs <clarabsouza@hotmail.com>
2023-12-30 01:34:09 -0300

    add prettier

--- src/lib/queryString/index.spec.js
+++ src/lib/queryString/index.spec.js
@@ -1,14 +1,14 @@
-const { queryString } = require(".")
+const { queryString } = require('.')
 
 describe('', () => {
-	it('should create a valid query string when an object', () => {
-		const userData = {
-			name: 'Maria',
-			role: 'Developer'
-		}
+  it('should create a valid query string when an object', () => {
+    const userData = {
+      name: 'Maria',
+      role: 'Developer',
+    }
 
-		const expectedQueryString = 'name=Maria&role=Developer'
+    const expectedQueryString = 'name=Maria&role=Developer'
 
-		expect(queryString(userData)).toBe(expectedQueryString)
-	})
+    expect(queryString(userData)).toBe(expectedQueryString)
+  })
 })
