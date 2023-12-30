b0c034c6ec156866986fa5b6c25682d742501ca5
mariaclarabs <clarabsouza@hotmail.com>
2023-12-30 00:50:01 -0300

    improve sum method and add more tests

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,5 +1,11 @@
 const sum = (firstNumber, secondNumber) => {
-  return firstNumber + secondNumber
+  const parsedFirstNumber = parseInt(firstNumber, 10)
+  const parsedSecondNumber = parseInt(secondNumber, 10)
+
+  if (isNaN(parsedFirstNumber) || isNaN(parsedSecondNumber))
+    throw new Error('Please provide two numbers to get the result')
+
+  return parsedFirstNumber + parsedSecondNumber
 }
 
 module.exports = { sum }
--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,10 +1,47 @@
 const { sum } = require("./calculator")
 
-it('should sum numbers and return the correct result in sum method', () => {
-  const firstNumber = 1
-  const secondNumber = 2
+describe('given sum method', () => {
+  it('should sum numbers and return the correct result', () => {
+    const firstNumber = 1
+    const secondNumber = 2
 
-  const expectedResult = firstNumber + secondNumber
+    const expectedResult = firstNumber + secondNumber
 
-  expect(sum(firstNumber, secondNumber)).toBe(expectedResult)
+    expect(sum(firstNumber, secondNumber)).toBe(expectedResult)
+  })
+
+  it('should sum and return the correct result even one of them is a string', () => {
+    const firstNumber = '1'
+    const secondNumber = '2'
+
+    const expectedResult = 3
+
+    expect(sum(firstNumber, secondNumber)).toBe(expectedResult)
+  })
+
+  it('should throw an error if provided parameters cannot be summed', () => {
+    expect(() => {
+      sum('', 1)
+    }).toThrow()
+
+    expect(() => {
+      sum([1, 2])
+    }).toThrow()
+
+    expect(() => {
+      sum({})
+    }).toThrow()
+
+    expect(() => {
+      sum()
+    }).toThrow()
+
+    expect(() => {
+      sum(undefined, 1)
+    }).toThrow()
+
+    expect(() => {
+      sum(null, 1)
+    }).toThrow()
+  })
 })
