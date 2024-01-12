41c56fb863337423b628db884127feb7d6001ef3
mariaclarabs <clarabsouza@hotmail.com>
2024-01-12 01:12:14 -0300

    improve and refactor query string method (object to string)

--- src/lib/queryString/index.js
+++ src/lib/queryString/index.js
@@ -1,6 +1,21 @@
-const queryString = (objectData) =>
-  Object.entries(objectData)
-    .map(([label, value]) => `${label}=${value}`)
-    .join('&')
+const isAnObject = (value) => {
+  return typeof value === 'object'
+}
+
+const isNotAnArray = (value) => {
+  return !Array.isArray(value)
+}
+
+const parseKeyValueToString = ([label, value]) => {
+  if (isAnObject(value) && isNotAnArray(value)) {
+    throw new Error('Object values are not supported')
+  }
+
+  return `${label}=${value}`
+}
+
+const queryString = (objectData) => {
+  return Object.entries(objectData).map(parseKeyValueToString).join('&')
+}
 
 module.exports = { queryString }
--- src/lib/queryString/index.spec.js
+++ src/lib/queryString/index.spec.js
@@ -1,7 +1,7 @@
 const { queryString } = require('.')
 
 describe('', () => {
-  it('should create a valid query string when an object', () => {
+  it('should create a valid query string when an object is provided', () => {
     const userData = {
       name: 'Maria',
       role: 'Developer',
@@ -10,5 +10,34 @@ describe('', () => {
     const expectedQueryString = 'name=Maria&role=Developer'
 
     expect(queryString(userData)).toBe(expectedQueryString)
+  })
+
+  it('should create a valid query string when an array is passed as value', () => {
+    const userData = {
+      name: 'Maria',
+      role: 'Developer',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+    }
+
+    const expectedQueryString =
+      'name=Maria&role=Developer&skills=HTML,CSS,JavaScript'
+
+    expect(queryString(userData)).toBe(expectedQueryString)
+  })
+
+  it('should throw an error when an object is passed as value', () => {
+    const userData = {
+      name: 'Maria',
+      role: 'Developer',
+      skills: {
+        first: 'HTML',
+        second: 'CSS',
+        third: 'JavaScript',
+      },
+    }
+
+    expect(() => {
+      queryString(userData)
+    }).toThrow()
   })
 })
